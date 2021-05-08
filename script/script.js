1bed4300897112709b52cbb906fcead982a671fa
Natalia Davydova <davydovanatalia063@gmail.com>
2021-05-08 13:12:14 -0700

    add function tabs

--- script/script.js
+++ script/script.js
@@ -103,4 +103,37 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   };
   togglePopUp();
+
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header");
+    const tabContent = document.querySelectorAll(".service-tab");
+    const tab = tabHeader.querySelectorAll(".service-header-tab");
+
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+
+    tabHeader.addEventListener("click", (e) => {
+      let target = e.target;
+      target = target.closest(".service-header-tab");
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+
+  tabs();
 });
